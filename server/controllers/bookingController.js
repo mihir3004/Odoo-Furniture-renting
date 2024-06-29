477b192c0d27b864e9dbeee23caadc08c930dd30
HarshN187 <hnirmal913@gmail.com>
2024-06-29 12:54:10 +0530

    boookin control

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,4 +1,26 @@
 const bookingModel = require("../models/bookingModel");
 const AppError = require("../utils/appError");
-
 const catchAsync = require("../utils/catchAsync");
+
+module.exports.createBooking = catchAsync(async (req, res, next) => {
+  const { user, furniture, startDate, endDate, totalAmount } = req.body;
+  const newBooking = new bookingModel({
+    user,
+    furniture,
+    startDate,
+    endDate,
+    totalAmount,
+    status: "pending", // Default status
+  });
+
+  const booking = await newBooking.save();
+  res.status(200).json({ booking });
+});
+
+module.exports.getBookings = catchAsync(async (req, res, next) => {
+  const bookings = await bookingModel
+    .find()
+    .populate("user")
+    .populate("furniture");
+  res.json(bookings);
+});
