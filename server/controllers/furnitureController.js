f33a7e72406886ca4dcfd3d435eda24ad82cba73
HarshN187 <hnirmal913@gmail.com>
2024-06-29 11:02:31 +0530

    furnitureController Done

--- server/controllers/furnitureController.js
+++ server/controllers/furnitureController.js
@@ -2,22 +2,31 @@ const furnitureModel = require("../models/furnitureModel");
 const catchAsync = require("../utils/catchAsync");
 
 module.exports.getAllFurniture = catchAsync(async (req, res, next) => {
-  const furniture = await Furniture.find();
+  const furniture = await furnitureModel.find(req.query);
   res.status(201).json({ status: "success", furniture });
 });
 
 module.exports.addFurniture = catchAsync(async (req, res, next) => {
-  const { name, description, price, image, category, location } = req.body;
+  const {
+    name,
+    description,
+    rentalPrice,
+    images,
+    available,
+    category,
+    rentalPeriod,
+    ownerId,
+  } = req.body;
 
   const newFurniture = new furnitureModel({
     name,
     description,
     rentalPrice,
     images,
+    available,
     category,
     rentalPeriod,
     ownerId,
-    location,
   });
 
   const furniture = await newFurniture.save();
