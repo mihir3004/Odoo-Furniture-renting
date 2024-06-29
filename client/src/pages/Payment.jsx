1bb1b59a7d4a44cbcf23cda3cb829652ec4dce3f
Dhruv9544 <dhruvshah905@gmail.com>
2024-06-29 15:59:12 +0530

    ui changes

--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -2,21 +2,23 @@ import { Button } from "primereact/button";
 import React from "react";
 import axios from "axios";
 function Payment({ furniture }) {
-    const payment = async () => {
-        // console.log(furniture);
-        localStorage.setItem("furniture", JSON.stringify(furniture));
-        let res = await axios.post("http://localhost:9999/payment", {
-            amount: furniture.rentalPrice,
-            name: furniture.name,
-        });
-        console.log(res);
-        window.location.href = res.data.url;
-    };
-    return (
-        <div>
-            <Button onClick={payment}>Pay Now</Button>
-        </div>
-    );
+  const payment = async () => {
+    // console.log(furniture);
+    localStorage.setItem("furniture", JSON.stringify(furniture));
+    let res = await axios.post("http://localhost:9999/payment", {
+      amount: furniture.rentalPrice,
+      name: furniture.name,
+    });
+    console.log(res);
+    window.location.href = res.data.url;
+  };
+  return (
+    <div>
+      <Button size="large" className="bg-green-500" onClick={payment}>
+        Pay Now
+      </Button>
+    </div>
+  );
 }
 
 export default Payment;
--- client/src/pages/User/FurnitureMainPage.jsx
+++ client/src/pages/User/FurnitureMainPage.jsx
@@ -4,60 +4,59 @@ import { useEffect, useState } from "react";
 import Payment from "../Payment";
 
 const FurnitureMainPage = () => {
-    const urlParams = useParams();
-    console.log(urlParams);
-    const [furniture, setFurniture] = useState([]);
-    const [loading, setloading] = useState(false);
+  const urlParams = useParams();
+  console.log(urlParams);
+  const [furniture, setFurniture] = useState([]);
+  const [loading, setloading] = useState(false);
 
-    async function getAllFurniture() {
-        const response = await fetchGet(
-            `furniture/getAll?_id=${urlParams.name}`,
-            localStorage.getItem("token")
-        );
-        if (response.status == "success") {
-            setFurniture(response.furnitures);
-            setloading(true);
-        }
-        console.log(response);
-    }
-    useEffect(() => {
-        getAllFurniture();
-        return () => {};
-    }, []);
-    return (
-        loading && (
-            <div class="bg-gray-100">
-                <div class="container mx-auto p-4">
-                    <div class="flex flex-col lg:flex-row">
-                        <div class="flex-1 p-4">
-                            <h1 class="text-4xl font-bold mb-2">
-                                {furniture[0].name}
-                            </h1>
-                            <p class="text-lg text-gray-700 mb-4">
-                                <i class="fas fa-map-marker-alt"></i>{" "}
-                                {furniture[0].ownerId.district},
-                                {furniture[0].ownerId.state}
-                            </p>
-                            <p class="text-blue-600 font-semibold mb-4">
-                                {furniture[0].description}
-                            </p>
-                            <p class="text-lg text-gray-700 mb-4">
-                                {furniture[0].rentalPrice / 100}
-                            </p>
-                            <Payment furniture={furniture[0]}></Payment>
-                        </div>
-                        <div class="flex-1 p-4">
-                            <img
-                                src={`http://localhost:9999/uploads/${furniture[0].images[0]}`}
-                                alt="Acme Fresh Start Housing"
-                                class="w-full h-auto object-cover rounded"
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
+  async function getAllFurniture() {
+    const response = await fetchGet(
+      `furniture/getAll?_id=${urlParams.name}`,
+      localStorage.getItem("token")
     );
+    if (response.status == "success") {
+      setFurniture(response.furnitures);
+      setloading(true);
+    }
+    console.log(response);
+  }
+  useEffect(() => {
+    getAllFurniture();
+    return () => {};
+  }, []);
+  return (
+    loading && (
+      <div>
+        <div class="container mx-auto p-8">
+          <div class="flex flex-col lg:flex-row items-center lg:items-start">
+            <div class="flex-1 p-4">
+              <h1 class="text-4xl font-bold text-gray-900 mb-4">
+                Name:{furniture[0].name}
+              </h1>
+              <p class="text-lg text-gray-700 flex items-center mb-4">
+                Location:{furniture[0].ownerId.district},{" "}
+                {furniture[0].ownerId.state}
+              </p>
+              <p class="text-blue-600 font-semibold mb-4">
+                description:{furniture[0].description}
+              </p>
+              <p class="text-xl text-gray-900 font-bold mb-6">
+                Price: ${furniture[0].rentalPrice / 100}
+              </p>
+              <Payment furniture={furniture[0]}></Payment>
+            </div>
+            <div class="flex-1 p-4 flex justify-center items-center">
+              <img
+                src={`http://localhost:9999/uploads/${furniture[0].images[0]}`}
+                alt="Furniture Image"
+                class="w-auto h-96 max-w-full object-cover rounded-lg shadow-lg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default FurnitureMainPage;
