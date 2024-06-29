539c80b630322b832bb25a85d02cd8f33dda04fb
Mihir Patel <mihirpatel1426@gmail.com>
2024-06-29 12:02:56 +0530

    add furniture

--- server/server.js
+++ server/server.js
@@ -9,37 +9,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/auth", userRoutes);
-app.use("/", furnitureRoutes);
+app.use("/furniture", furnitureRoutes);
 connectDB();
 app.post("/payment", async (req, res) => {
-  const product = await stripe.products.create({
-    name: "T-shirt",
-  });
-  if (product) {
-    var price = await stripe.prices.create({
-      product: `${product.id}`,
-      unit_amount: "10000",
-      currency: "inr",
+    const product = await stripe.products.create({
+        name: "T-shirt",
     });
-    if (price.id) {
-      var session = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            price: `${price.id}`,
-            quantity: 1,
-          },
-        ],
-        mode: "payment",
-        success_url: "http://localhost:3000/success",
-        cancel_url: "http://localhost:3000/error",
-        customer_email: "demo@gmail.com",
-      });
+    if (product) {
+        var price = await stripe.prices.create({
+            product: `${product.id}`,
+            unit_amount: "10000",
+            currency: "inr",
+        });
+        if (price.id) {
+            var session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+                        price: `${price.id}`,
+                        quantity: 1,
+                    },
+                ],
+                mode: "payment",
+                success_url: "http://localhost:3000/success",
+                cancel_url: "http://localhost:3000/error",
+                customer_email: "demo@gmail.com",
+            });
+        }
+        res.json(session);
     }
-    res.json(session);
-  }
 });
 
 const PORT = process.env.PORT || 9999;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
